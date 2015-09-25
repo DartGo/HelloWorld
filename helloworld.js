c63ed09dd01503c4d46c91d816b25a0a1eaf7f7b
DartGo <javagear@gmail.com>
2015-09-25 10:27:29 +0800

    no message

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,20 @@
-
 var page = tabris.create("Page", {
-  title: "Creating multiple top level pages",
+  title: "Hello, World!",
   topLevel: true
-}).open();
+});
 
-tabris.create("TextView", {
-  text: "Hello, world.",
-  layoutData: {centerX: 0, centerY: 0}
+var button = tabris.create("Button", {
+  text: "Native Widgets",
+  layoutData: {centerX: 0, top: 100}
 }).appendTo(page);
+
+var label = tabris.create("TextView", {
+  font: "24px",
+  layoutData: {centerX: 0, top: [button, 50]}
+}).appendTo(page);
+
+button.on("select", function() {
+  label.set("text", "Totally Rock!");
+});
+
+page.open();
